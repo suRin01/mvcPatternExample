f3d6585aa99b822408777bdb74f6ebf1d6023a1a
suRin01 <soullog77@gmail.com>
2021-07-16 16:15:16 +0900

    feat: 데이터베이스 연결 기능 완성 CRUD 기능 구현 해야 할 일: 데이터베이스 예외 처리

--- util/database.js
+++ util/database.js
@@ -19,18 +19,52 @@ function excute(query, data){
       );
 }
 
-function getAll(){
+function getUserList(){
     connection.query(
-        'SELECT * FROM `Users`',
+        'SELECT * FROM `MVC_test`.`Users`',
         function(err, results, fields) {
           console.log(results); // results contains rows returned by server
           console.log(fields); // fields contains extra meta data about results, if available
         }
       );
+}
 
+function insertUser(id, age, name){
+  connection.query(
+    "INSERT INTO `MVC_test`.`Users` (`id`, `age`, `name`) VALUES (?, ?, ?)",
+    [id, age, name],
+    function(err, results, fields){
+
+    }
+  )
+}
+
+function deleteUser(id){
+  connection.query(
+    "DELETE FROM `MVC_test`.`Users` WHERE (`id` = ?)",
+    id,
+    function(err, results, fields){
+
+    }
+  )
+}
+
+function patchUser(id, age){
+  connection.query(
+    "UPDATE `MVC_test`.`Users` SET `age` = ? WHERE (`id` = ?);",
+    [age, id],
+    function(err, results, fields){
+      
+    }
+  )
 }
 
 module.exports = {
-    excute: excute
+    excute: excute,
+    getUserList: getUserList,
+    insertUser: insertUser,
+    deleteUser: deleteUser,
+    patchUser: patchUser
+
 }
 
